c8233b470956496ae01e27087e7c8f5081d43f32
Marvin King <king9sky@126.com>
2019-02-22 21:43:51 +0800

    Finish all Functional_tests

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,22 +3,47 @@
 # @Author  : Marvin King
 # Date     : 2019-02-22
 from .base import FunctionalTest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
+
 
 class ItemValidationTest(FunctionalTest):
 
-
-    def can_not_add_empty_list_items(self):
+    def test_can_not_add_empty_list_items(self):
         # 乔伊访问首页，不小心提交一个空待办事项
         # 输入框中没输入内容，她就按下了回车
+        self.browser.get(self.live_server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # 首页刷新了，显示了一个错误信息
         # 提示待办事项不能为空
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
         # 她输入了一些文字，然后再次提交，这次没有问题了
+        self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
 
         # 她有点儿调皮，又提交了一个空待办事项
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # 在清单页她看到了一个类似的错误消息
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text,
+            "You can't have an empty list item"
+        ))
 
         # 输入文字之后就没问题了
-        self.fail('wirte me!')
+        self.browser.find_element_by_id('id_new_item').send_keys('Make tea')
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('1: Make tea')
+
